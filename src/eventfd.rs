e5671efc21dd07c98172087a37305a26baf78f6b
Jing Liu <jing2.liu@linux.intel.com>
2019-03-04 19:22:03 +0800

    vmm-sys-util: Add impl for crate eventfd
    Add FromRawFd impl for EventFd for the use of tests.
    
    Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

--- src/eventfd.rs
+++ src/eventfd.rs
@@ -92,6 +92,14 @@ impl AsRawFd for EventFd {
     }
 }
 
+impl FromRawFd for EventFd {
+    unsafe fn from_raw_fd(fd: RawFd) -> Self {
+        EventFd {
+            eventfd: File::from_raw_fd(fd),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
